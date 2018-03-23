232c0d040976ea7f08279798da97fcd013bb983e
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-23 23:18:12 +0800

    Added data plugins

--- pkg/cmd/grafana-server/public/app/plugins/datasource/fzakaria-simple-annotations-plugin-2df0c9c/module.ts
+++
@@ -1,4 +0,0 @@
-import { AnnotationDatasource } from './src/datasource';
-import { AnnotationsQueryCtrl } from './src/annotation_ctrl';
-
-export { AnnotationDatasource as Datasource, AnnotationsQueryCtrl };
